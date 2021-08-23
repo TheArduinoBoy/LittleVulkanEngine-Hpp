ad8aa166155325cf521ed0929b496e14c4342fcf
Linkthehero2222 <61379261+Linkthehero2222@users.noreply.github.com>
2021-08-23 13:31:10 -0400

    Add files via upload

---
+++ Engine.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Device.hpp"
+#include "Pipeline.hpp"
+#include "SwapChain.hpp"
+#include "Window.hpp"
+
+// std
+#include <memory>
+#include <vector>
+
+namespace Engine {
+    class Engine {
+        public:
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
+
+        Engine();
+        ~Engine();
+
+        Engine(const Engine &) = delete;
+        Engine &operator=(const Engine &) = delete;
+
+        void run();
+
+        private:
+        void createPipelineLayout();
+        void createPipeline();
+        void createCommandBuffers();
+        void drawFrame();
+
+        Window window{WIDTH, HEIGHT, "Hello Vulkan!"};
+        Device device{window};
+        SwapChain swapChain{device, window.getExtent()};
+        std::unique_ptr<Pipeline> pipeline;
+        vk::PipelineLayout pipelineLayout;
+        std::vector<vk::CommandBuffer> commandBuffers;
+    };
+}
