859e501adcbbd0d8dcbf241d48b636b8e5590e1e
Linkthehero2222 <61379261+Linkthehero2222@users.noreply.github.com>
2021-10-07 16:47:07 -0400

    Tutorial 13

---
+++ Engine.cpp
@@ -0,0 +1,106 @@
+#include "Engine.hpp"
+
+#include "Camera.hpp"
+#include "RenderSystem.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+// std
+#include <array>
+#include <stdexcept>
+
+namespace Engine {
+    Engine::Engine() {
+        loadGameObjects();
+    }
+
+    Engine::~Engine() {}
+
+    void Engine::run() {
+        RenderSystem renderSystem{device, renderer.getSwapChainRenderPass()};
+        Camera camera{};
+
+        while (!window.shouldClose()) {
+            glfwPollEvents();
+
+            float aspect = renderer.getAspectRatio();
+            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+
+            if(auto commandBuffer = renderer.beginFrame()) {
+                renderer.beginSwapChainRenderPass(commandBuffer);
+                renderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
+                renderer.endSwapChainRenderPass(commandBuffer);
+                renderer.endFrame();
+            }
+        }
+
+        device.device().waitIdle();
+    }
+
+    std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
+        std::vector<Model::Vertex> vertices{
+            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+            {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+            {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+        
+            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+            {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+            {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+            {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+            {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+        
+            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+            {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+            {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+        
+            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+            {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+            {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+        
+            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+            {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+            {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+        
+        };
+
+        for (auto& v : vertices) {
+            v.position += offset;
+        }
+
+        return std::make_unique<Model>(device, vertices);
+    }    
+
+    void Engine::loadGameObjects() {
+        std::shared_ptr<Model> model = createCubeModel(device, {.0f, .0f, .0f});
+
+        auto cube = GameObject::createGameObject();
+        cube.model = model;
+        cube.transform.translation = {.0f, .0f, 2.5f};
+        cube.transform.scale = {.5f, .5f, .5f};
+
+        gameObjects.push_back(std::move(cube));
+    }
+}
